4672b02e1ee8db9befe30c89b6fc4593eabdf5b2
Yifan Liu <liuyifan1030@outlook.com>
2025-06-13 23:09:35 +0800

    update c/cpp lsp config

--- nvim-minimum/.config/nvim/lsp/clangd.lua
+++ nvim-minimum/.config/nvim/lsp/clangd.lua
@@ -2,4 +2,23 @@ return {
     cmd = { 'clangd' },
     root_makers = { '.clangd', 'compile_commands.json' },
     filetypes = { 'c', 'cpp' },
+
+    -- custom keymap
+    vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+            local nmap = function(keys, func, desc)
+                if desc then
+                  desc = 'LSP: ' .. desc
+                end
+
+                vim.keymap.set('n', keys, func, { buffer = args.buf, desc = desc })
+            end
+
+          nmap('gd', vim.lsp.buf.definition, '[G] to [D]efinition')
+          nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+          nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+        end
+    }),
+
 }
